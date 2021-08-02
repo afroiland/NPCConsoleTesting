f81b6e25412f8668c78d318f4e6f7991a9ea40dc
afroiland <andrew.froiland@gmail.com>
2021-08-02 14:23:03 -0500

    inital commit

---
+++ NPCConsoleTesting/Character.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPCConsoleTesting
+{
+    class Character
+    {
+        int hp;
+        int initMod;
+        int ac;
+        int thac0;
+        public int calcDmg()
+        {
+            Random _random = new Random();
+            return _random.Next(1, 6);
+        }
+                
+    }
+}
