8477a124b80302d9806c93756645f9df31d10369
afroiland <andrew.froiland@gmail.com>
2021-08-03 11:50:40 -0500

    create Build and Combat classes, move code to Combat class

--- NPCConsoleTesting/Program.cs
+++ NPCConsoleTesting/Program.cs
@@ -6,28 +6,8 @@ namespace NPCConsoleTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter HP for character1");
-            int npc1HP = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter initMod for character1");
-            int npc1InitMod = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter AC for character1");
-            int npc1AC = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter thac0 for character1");
-            int npc1Thac0 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine($"character1 HP: {npc1HP}, character1 initMod: {npc1InitMod}, " +
-                $"character1 AC: {npc1AC}, character1 thac0: {npc1Thac0}");
-
-            //user enters stats for NPC2
-
-            //character objects are hydrated with above data
-            Character npc1 = new Character(npc1HP, npc1InitMod, npc1AC, npc1Thac0);
-
-            
-            //Console.WriteLine($"{npc1.calcDmg()}");
+            Build _builder = new Build();
+            _builder.BuildCharacter();
 
             //do a fight
 
@@ -36,7 +16,7 @@ namespace NPCConsoleTesting
             Console.ReadLine();
         }
 
-        public int calcDmg(int numberOfDice, int typeOfDie, int modifier)
+        public int CalcDmg(int numberOfDice, int typeOfDie, int modifier)
         {
             Random _random = new Random();
             int result = 0;
