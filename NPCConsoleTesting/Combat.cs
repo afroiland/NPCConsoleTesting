ff613d6aa62c49dcc00d1fbedc935c9169c0ddf7
afroiland <andrew.froiland@gmail.com>
2021-08-03 13:42:13 -0500

    clean up BuildCharacter, move calcDmg to Combat class

--- NPCConsoleTesting/Combat.cs
+++ NPCConsoleTesting/Combat.cs
@@ -8,9 +8,22 @@ namespace NPCConsoleTesting
 {
     class Combat
     {
-        public void Fight()
+        public void Fight(Character char1, Character char2)
         {
             Console.WriteLine("test");
+        }
+
+        public int CalcDmg(int numberOfDice, int typeOfDie, int modifier)
+        {
+            Random _random = new Random();
+            int result = 0;
+
+            for (int i = 0; i < numberOfDice; i++)
+            {
+                result += _random.Next(1, typeOfDie);
+            }
+
+            return result + modifier;
         }
     }
 }
