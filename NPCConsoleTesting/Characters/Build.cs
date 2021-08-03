3bc5486a0f12c5ccd492778ff164e39d0ef9b155
afroiland <andrew.froiland@gmail.com>
2021-08-03 16:39:32 -0500

    organize classes, format console output

--- NPCConsoleTesting/Characters/Build.cs
+++ NPCConsoleTesting/Characters/Build.cs
@@ -31,31 +31,7 @@ namespace NPCConsoleTesting
             Console.WriteLine("Enter modifier for character1");
             int npcModifier = int.Parse(Console.ReadLine());
 
-            //user enters stats for NPC2
-            //Console.WriteLine("Enter HP for character2");
-            //int npc2HP = int.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Enter initMod for character2");
-            //int npc2InitMod = int.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Enter AC for character2");
-            //int npc2AC = int.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Enter thac0 for character2");
-            //int npc2Thac0 = int.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Enter numberOfDice for character2");
-            //int npc2NumberOfDice = int.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Enter typeOfDie for character2");
-            //int npc2TypeOfDie = int.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Enter modifier for character2");
-            //int npc2Modifier = int.Parse(Console.ReadLine());
-
-            //character objects are hydrated with above data
             Character npc = new(npcHP, npcInitMod, npcAC, npcThac0, npcNumberOfDice, npcTypeOfDie, npcModifier);
-            //Character npc2 = new(npc2HP, npc2InitMod, npc2AC, npc2Thac0, npc2NumberOfDice, npc2TypeOfDie, npc2Modifier);
 
             return npc;
         }
--- NPCConsoleTesting/Program.cs
+++ NPCConsoleTesting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NPCConsoleTesting
 {
@@ -15,19 +16,20 @@ namespace NPCConsoleTesting
             Console.WriteLine($"character1 AC: {npc1.ac}");
             Console.WriteLine($"character1 thac0: {npc1.thac0}");
             Console.WriteLine($"character1 dmg: {npc1.numberOfDice}d{npc1.typeOfDie}+{npc1.modifier}");
+            Console.WriteLine();
 
             Console.WriteLine($"character2 HP: {npc2.hp}");
             Console.WriteLine($"character2 initMod: {npc2.initMod}");
             Console.WriteLine($"character2 AC: {npc2.ac}");
             Console.WriteLine($"character2 thac0: {npc2.thac0}");
             Console.WriteLine($"character2 dmg: {npc2.numberOfDice}d{npc2.typeOfDie}+{npc2.modifier}");
+            Console.WriteLine();
 
             //do a fight
             Combat combat = new();
-            combat.Fight(npc1, npc2);
+            var log = new List<String>(combat.Fight(npc1, npc2));
 
-            //print results/combat log
-
+            log.ForEach(i => Console.WriteLine(i));
             Console.ReadLine();
         }
     }
