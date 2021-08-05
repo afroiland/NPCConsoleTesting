b43bfbfe008ad4da8f523f46931d09a97064ff84
afroiland <andrew.froiland@gmail.com>
2021-08-04 21:09:55 -0500

    move Random to class level, sketch out functionality

--- NPCConsoleTesting/Combat/Combat.cs
+++ NPCConsoleTesting/Combat/Combat.cs
@@ -8,6 +8,8 @@ namespace NPCConsoleTesting
 {
     class Combat
     {
+        static Random _random = new();
+
         public static List<String> Fight(Character char1, Character char2)
         {
             Character tempChar1 = char1;
@@ -15,16 +17,38 @@ namespace NPCConsoleTesting
             //var combatLog = new List<String>();
             var combatLog = new List<String> { "test1", "test2", "test3" };
 
-            //whole thing in an if stmt from here?
+            //Random _random = new();
+
+            //whole thing in an if/while stmt from here?
 
             //determine init
-
+            var char1Init = _random.Next(1, 10) + char1.initMod;
+            var char2Init = _random.Next(1, 10) + char2.initMod;
+            //Console.WriteLine($"char1Init: {char1Init}");
+            //Console.WriteLine($"char2Init: {char2Init}");
+            //Console.ReadLine();
 
             //first char attack (attack could be a separate method, just returns a pos or neg int?)
+            bool simultaneous = char1Init == char2Init ? true : false;
 
-            //check hp
+            //find char w/ lowest init
+            
 
-            //second char attack
+
+            //string attackerName = attacker.name;
+            //string defenderName = defender.name;
+
+            //that char does an attack
+            //int attackResult = Attack(attackerName.thac0, defenderName.ac, attackerName.numberOfDice, attackerName.typeOfDie, attackerName.modifier);
+
+
+            //adjust defender hp
+            //check hp of defender
+            //if hp <= 0
+            //      if simultaneous = false, fight over
+            //next char get a turn      
+
+            //next char attack
 
             //check hp
 
@@ -34,9 +58,20 @@ namespace NPCConsoleTesting
             return combatLog;
         }
 
+        public int Attack(int thac0, int ac, int numberOfDice, int typeOfDie, int modifier)
+        {
+            int result = new();
+
+            //calc if hit
+            result = thac0 > ac + _random.Next(1, 20) ? 0 : CalcDmg(numberOfDice, typeOfDie, modifier);
+
+            //determine dmg
+            return result;
+        }
+
         public int CalcDmg(int numberOfDice, int typeOfDie, int modifier)
         {
-            Random _random = new Random();
+            //Random _random = new();
             int result = 0;
 
             for (int i = 0; i < numberOfDice; i++)
