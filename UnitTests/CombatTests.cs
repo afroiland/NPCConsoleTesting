c877b66ff8060a31f48364dc0ddeb8e2a20f7743
afroiland <andrew.froiland@gmail.com>
2021-08-05 21:12:37 -0500

    Add UnitTests project

---
+++ UnitTests/CombatTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class CombatTests
+    {
+        //[SetUp]
+        //public void Setup()
+        //{
+        //}
+
+        [Test]
+        public void Targets_Get_Set_for_All_Chars()
+        {
+            //arrange
+
+
+            //act
+            
+            
+            //assert
+            Assert.Pass();
+        }
+    }
+}
