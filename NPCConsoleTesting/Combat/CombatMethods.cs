084367b9376d44d0f70a9984fc48d30719b91cbd
afroiland <andrew.froiland@gmail.com>
2021-08-09 11:43:12 -0500

    adjust range for random, add tests for CalcDmg and Attack

--- NPCConsoleTesting/Combat/CombatMethods.cs
+++ NPCConsoleTesting/Combat/CombatMethods.cs
@@ -14,7 +14,7 @@ namespace NPCConsoleTesting
 
         public static int Attack(int thac0, int ac, int numberOfDice, int typeOfDie, int modifier)
         {
-            return thac0 > ac + _random.Next(1, 20) ? 0 : CalcDmg(numberOfDice, typeOfDie, modifier);
+            return thac0 > ac + _random.Next(1, 21) ? 0 : CalcDmg(numberOfDice, typeOfDie, modifier);
         }
 
         public static int CalcDmg(int numberOfDice, int typeOfDie, int modifier)
@@ -23,7 +23,7 @@ namespace NPCConsoleTesting
 
             for (int i = 0; i < numberOfDice; i++)
             {
-                result += _random.Next(1, typeOfDie);
+                result += _random.Next(1, typeOfDie + 1);
             }
 
             return result + modifier;
@@ -34,15 +34,9 @@ namespace NPCConsoleTesting
             //set inits
             foreach (Character ch in chars)
             {
-                ch.init = _random.Next(1, 10) + ch.initMod;
+                ch.init = _random.Next(1, 11) + ch.initMod;
                 //ch.init = 5;
             }
-            //show inits
-            //for (int i = 0; i < chars.Count; i++)
-            //{
-            //    Console.WriteLine($"{chars[i].name} init: {chars[i].init}");
-            //}
-            //if (doReadLines) { Console.ReadLine(); }
 
             //order chars by init
             //chars = chars.OrderBy(x => x.init).ToList();
@@ -66,7 +60,7 @@ namespace NPCConsoleTesting
                 if (ch.target == "" || chars.Where(x => x.name == ch.target).Select(x => x.hp).ToList()[0] <= 0)
                 {
                     List<string> potentialTargets = chars.Where(x => ch.name != x.name && x.hp > 0).Select(x => x.name).ToList();
-                    ch.target = potentialTargets[_random.Next(0, potentialTargets.Count - 1)];
+                    ch.target = potentialTargets[_random.Next(0, potentialTargets.Count)];
                 }
             }
             //show targets
--- UnitTests/CombatTests.cs
+++ UnitTests/CombatTests.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 namespace UnitTests
 {
-    //[TestFixture]
     public class CombatTests
     {
         //Arrange
@@ -16,7 +15,61 @@ namespace UnitTests
         };
 
         [Test]
-        public void Targets_Get_Set_for_All_Chars()
+        public void Attack_succeeds_and_fails_as_expected()
+        {
+            //Arrange
+            int thac0 = 11;
+            int poorAC = 10;
+            int goodAC = -10;
+            int numOfDice = 1;
+            int typeOfDie = 6;
+            int modifier = 2;
+            List<int> resultsListPoorAC = new();
+            List<int> resultsListGoodAC = new();
+
+            //Act
+            for (int i = 0; i < 50; i++)
+            {
+                resultsListPoorAC.Add(CombatMethods.Attack(thac0, poorAC, numOfDice, typeOfDie, modifier));
+                resultsListGoodAC.Add(CombatMethods.Attack(thac0, goodAC, numOfDice, typeOfDie, modifier));
+            }
+
+            //Assert
+            Assert.That(resultsListPoorAC, Is.All.GreaterThan(0));
+            Assert.That(resultsListGoodAC, Is.All.LessThan(1));
+        }
+
+        [Test]
+        public void CalcDmg_falls_within_range()
+        {
+            //Arrange
+            int numOfDice = 1;
+            int typeOfDie = 6;
+            int modifier = 2;
+            List<int> resultsList = new();
+
+            //Act
+            for (int i = 0; i < 50; i++)
+            {
+                resultsList.Add(CombatMethods.CalcDmg(numOfDice, typeOfDie, modifier));
+            }
+
+            //Assert
+            Assert.That(resultsList, Is.All.GreaterThan(2) & Is.All.LessThan(9) & Has.Member(3) & Has.Member(8));
+        }
+
+        [Test]
+        public void Inits_get_set_for_all_chars()
+        {
+            //Act
+            var result = CombatMethods.DetermineInit(testList);
+
+            //Assert
+            Assert.That(result, Is.Ordered.By("init"));
+        }
+
+        [Test]
+        public void Targets_get_set_for_all_chars()
         {
             //Act
             var result = CombatMethods.DetermineTargets(testList);
@@ -29,16 +82,6 @@ namespace UnitTests
 
             //TODO: The following seems cleaner once "target" has been made into a property
             //Assert.That(List.Map(result).Property("target"), Is.Not.EqualTo(""));
-        }
-
-        [Test]
-        public void Inits_Get_Set_for_All_Chars()
-        {
-            //Act
-            var result = CombatMethods.DetermineInit(testList);
-
-            //Assert
-            Assert.That(result, Is.Ordered.By("init"));
         }
     }
 }
