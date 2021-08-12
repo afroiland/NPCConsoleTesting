e19039019c5b65e76af078407d813061b7a9a7d4
afroiland <andrew.froiland@gmail.com>
2021-08-12 13:20:56 -0500

    Make Combatant properties, expand Character class

--- NPCConsoleTesting/Characters/Character.cs
+++ NPCConsoleTesting/Characters/Character.cs
@@ -6,8 +6,46 @@ using System.Threading.Tasks;
 
 namespace NPCConsoleTesting
 {
-    class Character
+    public class Character
     {
+        public string name;
+        public string title;
+        public int level;
+        public string characterClass;
+        public string race;
+        public int age;
+        public string gender;
+        public int currentHP;
+        public List<int> hpByLevel;
+        public int ac_adj;
+        public int att_adj;
+        public string status;
+        public int str;
+        public int ex_str;   //unsure if it's worth to make a child class for this one property
+        public int intel;
+        public int dex;
+        public int con;
+        public int wis;
+        public int cha;
+        public int gold;
+        public string armor;
+        public string weapon;
+        public string items;
+        public int probity;
+        public string affiliation;
+        public string notes;
 
+        //spellbook/memorized in a separate class? Or child classes for MU, cleric, etc.
+
+        //The following not needed? Determined during combat?
+        public int thac0;
+        public int numberOfDice;
+        public int typeOfDie;
+        public int modifier;
+
+        public Character()
+        {
+
+        }
     }
 }
