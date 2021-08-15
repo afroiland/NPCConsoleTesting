f07d3fb131d41052bf630294e2515e7de3f1b6f6
afroiland <andrew.froiland@gmail.com>
2021-08-15 10:47:16 -0500

    Add GenerateRandomName method

--- NPCConsoleTesting/Characters/Build.cs
+++ NPCConsoleTesting/Characters/Build.cs
@@ -8,11 +8,12 @@ namespace NPCConsoleTesting
 {
     class Build
     {
+        static Random _random = new();
+
         public static Combatant BuildCombatantRandomly()
         {
-            Random _random = new();
-
-            string name = "random" + _random.Next(10000, 100000).ToString();
+            string name = GenerateRandomName();
+            //string name = "random" + _random.Next(10000, 100000).ToString();
             int HP = _random.Next(1, 11);
             int initMod = _random.Next(1, 6);
             int AC = _random.Next(-10, 11);
@@ -51,6 +52,70 @@ namespace NPCConsoleTesting
             int modifier = int.Parse(Console.ReadLine());
 
             return new(name, HP, initMod, AC, thac0, numberOfDice, typeOfDie, modifier);
+        }
+
+        public static string GenerateRandomName()
+        {
+            //TODO: This method could use a lot of cleanup. Enum, comments, etc.
+            string[] consonants = {"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "qu", "r", "s", "t", "v", "w", "x", "z"};
+            string[] startingBlends = {"bl", "br", "cl", "cr", "dr", "fl", "fr", "gl", "gr", "pl", "pr", "sl",
+                "sn", "sw", "tr", "tw", "wh", "wr", "scr", "shr", "sph", "spl", "spr", "squ", "str", "thr"};
+            string[] endingBlends = { "ch", "sc", "sh", "sk", "sm", "sp", "st", "th", "sch"};
+            string[] vowels = {"a", "e", "i", "o", "u", "y"};
+            string[] doubleVowels = {"aa", "ae", "ai", "ao", "au", "ea", "ee", "ei", "eo", "eu", "ia", "ie",
+                "io", "iu", "oa", "oe", "oi", "oo", "ou", "ua", "ue", "ui", "uo"};
+
+            int patternLength = _random.Next(3, 7);
+            List<int> pattern = new() {_random.Next(1, 5)};
+            string name = new("");
+
+            //determine pattern
+            while (pattern.Count < patternLength)
+            {
+                if (pattern[pattern.Count - 1] != 4)
+                {
+                    pattern.Add(4);
+                }
+                else
+                {
+                    pattern.Add(_random.Next(1, 4));
+                }
+                if (pattern[pattern.Count - 1] == 2)
+                {
+                    pattern.RemoveAt(pattern.Count - 1);
+                }
+            }
+
+            //add letter strings according to pattern
+            for (int i = 0; i < pattern.Count; i++)
+            {
+                switch (pattern[i])
+                {
+                    case 1:
+                        name += consonants[_random.Next(1, consonants.Length - 1)];
+                        break;
+                    case 2:
+                        name += startingBlends[_random.Next(1, startingBlends.Length - 1)];
+                        break;
+                    case 3:
+                        name += endingBlends[_random.Next(1, endingBlends.Length - 1)];
+                        break;
+                    case 4:
+                        if (_random.Next(1, 11) > 4)
+                        {
+                            name += vowels[_random.Next(1, vowels.Length - 1)];
+                        }
+                        else
+                        {
+                            name += doubleVowels[_random.Next(1, doubleVowels.Length - 1)];
+                        }
+                        break;
+                    default: break;
+                }
+            }
+
+            //Capitalize first letter and return
+            return char.ToUpper(name[0]) + name[1..];
         }
     }
 }
