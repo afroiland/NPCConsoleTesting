3509ab1cf65c76fb9236325861ac5dfdbbf3a8f6
afroiland <andrew.froiland@gmail.com>
2021-08-23 20:24:58 -0500

    connect to sql server

---
+++ NPCConsoleTesting/Models/CombatantModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPCConsoleTesting.Models
+{
+    class CombatantModel
+    {
+        string Name { get; set; }
+        int HP { get; set; }
+        int InitMod { get; set; }
+        int AC { get; set; }
+        int Thac0 { get; set; }
+        int NumberOfAttackDice { get; set; }
+        int TypeOfAttackDie { get; set; }
+        int DmgModifier { get; set; }
+        int Init { get; set; }
+        string Target { get; set; }
+    }
+}
